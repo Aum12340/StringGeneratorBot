7ba1fe92b0d39f81e8a03021db3b1f7045e65101
WARFADE <96909243+WARFADE-BOSS@users.noreply.github.com>
2022-01-24 15:34:26 +0530

    Add files via upload

---
+++ Data.py
@@ -0,0 +1,61 @@
+from pyrogram.types import InlineKeyboardButton
+
+
+class Data:
+    # Start Message
+    START = """
+Hey {}, 
+Welcome to {}
+
+You Can Use This Bot To Generate Pyrogram & Telethon String Session. Use Below Buttons To Operate !
+
+**Made With Love By @WarFade ❤️**
+    """
+
+    # Home Button
+    home_buttons = [
+        [InlineKeyboardButton("🔥 Start Generating Session 🔥", callback_data="generate")],
+        [InlineKeyboardButton(text="🏠 Return Home 🏠", callback_data="home")]
+    ]
+
+    generate_button = [
+        [InlineKeyboardButton("🔥 Start Generating Session 🔥", callback_data="generate")]
+    ]
+
+    # Rest Buttons
+    buttons = [
+        [InlineKeyboardButton("🔥 Start Generating Session 🔥", callback_data="generate")],
+        [
+            InlineKeyboardButton("How to Use ❔", callback_data="help"),
+            InlineKeyboardButton("🎪 About 🎪", callback_data="about")
+        ],
+    ]
+
+    # Help Message
+    HELP = """
+✨ **Available Commands** ✨
+
+/about - About The Bot
+/help - This Message
+/start - Start the Bot
+/generate - Start Generating Session
+/cancel - Cancel the process
+/restart - Cancel the process
+"""
+
+    # About Message
+    ABOUT = """
+**About This Bot** 
+
+A Telegram Bot To Generate Pyrogram & Telethon String Sessions. 
+
+Made With ❤ By @WarFade
+
+Sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ : [Click Here](https://t.me/WarFade)
+
+Fʀᴀᴍᴇᴡᴏʀᴋ : [Pyrogram](docs.pyrogram.org)
+
+Lᴀɴɢᴜᴀɢᴇ : [Python](www.python.org)
+
+Dᴇᴠᴇʟᴏᴘᴇʀ : @WarFade
+    """
